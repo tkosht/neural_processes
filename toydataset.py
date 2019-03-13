f55fa97b621cf8bd16af858445ff2fb933804dc2
tkosht <takehito.oshita.business@gmail.com>
2019-03-14 02:04:00 +0900

    updated: refactoring.

--- toydataset.py
+++ toydataset.py
@@ -38,49 +38,6 @@ class GPCurvesReader(object):
         kernel += (sigma_noise**2) * torch.eye(num_total_points)
         return kernel
 
-####    def generate_curves2(self, C=120, T=400):
-####        # num_context = torch.randint(3, self.max_num_context, size=(1,))[0]
-####        assert C <= T
-####        num_context = C
-####        num_target = T
-####
-####        num_total_points = num_target
-####        x_values = torch.arange(-2., 2., 1./100).unsqueeze(0).repeat(self._batch_size, 1)
-####        x_values = x_values.unsqueeze(-1)
-####
-####        if self._random_kernel_parameters:
-####            l1 = torch.empty(self._batch_size, self.y_size, self.x_size).uniform_(0.1, self._l1_scale)
-####            sigma_f = torch.empty(self._batch_size, self.y_size).uniform_(0.1, self._sigma_scale)
-####        else:
-####            l1 = torch.ones([self._batch_size, self._y_size, self._x_size]) * self._l1_scale
-####            sigma_f = torch.ones([self._batch_size, self._y_size]) * self._sigma_scale
-####
-####        kernel = self._gaussian_kernel(x_values, l1, sigma_f)
-####        cholesky = torch.cholesky(kernel.type(torch.DoubleTensor)).type(torch.FloatTensor)
-####
-####        eps = torch.normal(torch.zeros([self._batch_size, self.y_size, num_total_points, 1]))
-####        y_values = torch.matmul(cholesky, eps)     # multivariate normal
-####        y_values = y_values.squeeze(-1).transpose(1, 2)
-####
-####        target_x = x_values
-####        target_y = y_values
-####        idx = torch.randperm(num_target)
-####        idx_tensor = idx[:num_context]
-####        context_x = x_values.index_select(1, idx_tensor)
-####        context_y = y_values.index_select(1, idx_tensor)
-####
-####        context_x = context_x.to(self._device)
-####        context_y = context_y.to(self._device)
-####        target_x = target_x.to(self._device)
-####        target_y = target_y.to(self._device)
-####        query = ((context_x, context_y), target_x)
-####
-####        return NPRegressionDescription(
-####            query=query,
-####            target_y=target_y,
-####            num_total_points=target_x.shape[1],
-####            num_context_points=num_context)
-
     def generate_curves(self):
         num_context = torch.randint(3, self.max_num_context, size=(1,))[0]
         if self._testing:
